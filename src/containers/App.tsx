c66357a54e987b8e9f109e735697a7bdf7b94e0c
krouspy <knjlau1@gmail.com>
2020-11-17 15:08:44 +0100

    move Header from Components to Containers

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.less';
 import { Layout } from 'antd';
-import { Header } from '@components/Header';
+import { Header } from './Header';
 
 const { Content } = Layout;
 

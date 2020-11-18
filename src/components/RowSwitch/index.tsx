9a63370217770a1b42f0c2ba1e989171df86abdf
krouspy <knjlau1@gmail.com>
2020-11-18 15:30:08 +0100

    Content: simplify RowSwitch

--- src/components/RowSwitch/index.tsx
+++ src/components/RowSwitch/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Row } from 'antd';
-import { ColumnText } from './ColumnText';
-import { ColumnImage } from './ColumnImage';
+import './index.less';
+import { Row, Col, Typography } from 'antd';
+// import { ColumnText } from './ColumnText';
+// import { ColumnImage } from './ColumnImage';
+
+const { Title, Paragraph } = Typography;
 
 interface RowInterface {
   title: string;
@@ -19,18 +22,22 @@ export const RowSwitch: React.FC<RowInterface> = ({
   isLastItem,
 }: RowInterface) => {
   return (
-    <Row justify="center" style={{ maxHeight: '450px' }}>
-      {reverse ? (
-        <>
-          <ColumnImage imageSource={imageSource} isLastItem={isLastItem} />
-          <ColumnText title={title} content={content} reverse />
-        </>
-      ) : (
-        <>
-          <ColumnText title={title} content={content} />
-          <ColumnImage imageSource={imageSource} isLastItem={isLastItem} />
-        </>
-      )}
+    <Row className="row-switch" justify="center">
+      <Col
+        className={reverse ? 'col-text-left' : 'col-text-right'}
+        span={12}
+        order={reverse ? 2 : 1}
+      >
+        <Title level={3}>{title}</Title>
+        <Paragraph className="paragraph">{content}</Paragraph>
+      </Col>
+      <Col
+        className={isLastItem ? 'col-img-last-item' : 'col-img'}
+        span={12}
+        order={reverse ? 1 : 2}
+      >
+        <img src={imageSource} alt="grill" />
+      </Col>
     </Row>
   );
 };
